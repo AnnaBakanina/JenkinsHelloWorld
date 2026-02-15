bd5f42402e60665770aeda1456b7c62c8e7bbc4b
AnnaBakanina <49923857+AnnaBakanina@users.noreply.github.com>
2026-02-15 18:13:04 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("-------**** Hello World - Jenkins Training ****-------");
         System.out.println("-------**** EDIT 1 ****-------");
+        System.out.println("-------**** EDIT 2 ****-------");
     }
 }
